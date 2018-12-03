f264e6356e216d60ae79abc9875da0bdd00f97a7
oscar <oo.ortega2247@gmail.com>
2018-12-02 21:54:28 -0600

    support for sbml and bngl files, and biomodels databse

--- pysbjupyter/model_simresult_to_json.py
+++ pysbjupyter/model_simresult_to_json.py
@@ -2,21 +2,39 @@ from pysbjupyter.static_viz import StaticViz
 from pysbjupyter.dynamic_viz import ModelVisualization
 from pysb.core import Model
 from pysb.simulator.base import SimulationResult
+from pysb.importers.bngl import model_from_bngl
+from pysb.importers.sbml import model_from_sbml, model_from_biomodels
+import os
 
 
 def data_to_json(_, widget):
     if isinstance(widget.data, Model):
         viz = StaticViz(widget.data)
-        try:
-            jsondata = getattr(viz, widget.type_of_viz)()
-        except AttributeError:
-            raise AttributeError('Type of visualization not defined')
-        return jsondata
+
+    elif isinstance(widget.data, str):
+        file_extension = os.path.splitext(widget.data)[1]
+        if file_extension == '.bngl':
+            model = model_from_bngl(widget.data)
+        elif file_extension == '.sbml':
+            model = model_from_sbml(widget.data)
+        elif widget.data.startswith('BIOMD'):
+            model = model_from_biomodels(widget.data)
+        else:
+            raise ValueError('Format not supported')
+        viz = StaticViz(model)
+
     elif isinstance(widget.data, SimulationResult):
         viz = ModelVisualization(widget.data)
-        jsondata = viz.dynamic_view(get_passengers=False)
-        return jsondata
+
     else:
         raise TypeError('Only Model and SimulationResult are supported')
+
+    try:
+        jsondata = getattr(viz, widget.type_of_viz)()
+    except AttributeError:
+        raise AttributeError('Type of visualization not defined')
+    return jsondata
+
+
 
 
