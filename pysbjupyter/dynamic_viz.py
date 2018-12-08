06915f0128d8f8269360f22e9e230a3ff2c7fa49
oscar <oo.ortega2247@gmail.com>
2018-12-08 17:02:54 -0600

    Added documentation to dynamic_viz module

--- pysbjupyter/dynamic_viz.py
+++ pysbjupyter/dynamic_viz.py
@@ -1,8 +1,5 @@
 from collections import OrderedDict
 import networkx as nx
-import json
-from .util_networkx import from_networkx
-import collections
 import sympy
 import numpy as np
 from pysb.simulator import SimulationResult
@@ -39,7 +36,7 @@ class MidpointNormalize(colors.Normalize):
 
 class ModelVisualization(object):
     """
-    Class to visualize models and their dynamics
+    Class to visualize PySB models and their dynamics
 
     Parameters
     ----------
@@ -60,7 +57,7 @@ class ModelVisualization(object):
         self.passengers = []
         self.type_viz = ''
 
-    def dynamic_view(self, type_viz='consumption', get_passengers=True):
+    def dynamic_view(self, type_viz='consumption'):
         """
         Generates a dictionary with the model dynamics data that can be converted in the Cytoscape.js JSON format
 
@@ -68,20 +65,21 @@ class ModelVisualization(object):
         ----------
         type_viz : str
             Type of the dynamic visualization, it can be 'consumption' or 'production'
-        get_passengers : bool
-            if True, nodes that only have one incoming or outgoing edge are painted with a different color
-
 
         Examples
         --------
         >>> from pysb.examples.earm_1_0 import model
+        >>> from pysb.simulator import ScipyOdeSimulator
         >>> import numpy as np
-        >>> viz = ModelVisualization(model)
-        >>> data = viz.dynamic_view(tspan=np.linspace(0, 20000, 100))
+        >>> tspan = np.linspace(0, 20000)
+        >>> sim = ScipyOdeSimulator(model, tspan).run()
+        >>> viz = ModelVisualization(sim)
+        >>> data = viz.dynamic_view()
 
         Returns
         -------
-        A Dictionary Object that can be converted into Cytoscape.js JSON
+        A Dictionary Object with all nodes and edges information that
+        can be converted into Cytoscape.js JSON to be visualized
         """
         self.type_viz = type_viz
         self.sp_graph = StaticViz(self.model).species_graph()
@@ -121,38 +119,13 @@ class ModelVisualization(object):
             nx.set_node_attributes(self.sp_graph, 'abs_value', node_abs)
             nx.set_node_attributes(self.sp_graph, 'rel_value', node_rel)
 
-    @staticmethod
-    def _get_max_rrs(rxns_matrix_sp, diff_par):
-        # Tropicalization
-        sp_rr = rxns_matrix_sp
-        sp_rr_dom = np.zeros(sp_rr.shape)
-        # sp_rr_neg = np.zeros(sp_rr.shape)
-        for col in range(sp_rr.shape[1]):
-            # Indices of positive and negative rates at each time point
-            # log10 of the positive and negative reaction rates
-            pos_idx = np.where(sp_rr[:, col] > 0)[0]
-            neg_idx = np.where(sp_rr[:, col] < 0)[0]
-            rr_pos = np.log10(sp_rr[:, col][pos_idx])
-            rr_neg = np.log10(np.abs(sp_rr[:, col][neg_idx]))
-
-            if rr_neg.size != 0:
-                max_val_neg = np.amax(rr_neg)
-                dom_rr_neg = [neg_idx[n] for n, i in enumerate(rr_neg)
-                              if i > (max_val_neg - diff_par) and max_val_neg > -5]
-                if not dom_rr_neg:
-                    dom_rr_neg = neg_idx
-                sp_rr_dom[:, col][dom_rr_neg] = sp_rr[:, col][dom_rr_neg]
-
-            if rr_pos.size != 0:
-                max_val_pos = np.amax(rr_pos)
-                dom_rr_pos = [pos_idx[n] for n, i in enumerate(rr_pos)
-                              if i > (max_val_pos - diff_par) and max_val_pos > -5]
-                if not dom_rr_pos:
-                    dom_rr_pos = pos_idx
-                sp_rr_dom[:, col][dom_rr_pos] = sp_rr[:, col][dom_rr_pos]
-        return sp_rr_dom
-
     def matrix_bidirectional_rates(self):
+        """
+        Obtains the values of the reaction rates at all the time points of the simulation
+        Returns
+        -------
+        An np.ndarray with the reaction rates values
+        """
         rxns_matrix = np.zeros((len(self.model.reactions_bidirectional), len(self.tspan)))
 
         # Calculates matrix of bidirectional reaction rates
@@ -174,13 +147,15 @@ class ModelVisualization(object):
 
     def edges_colors_sizes(self):
         """
-
-        Returns
-        -------
         This function obtains values for the size and color of the edges in the network.
         The color is a representation of the percentage of flux going through an edge.
         The edge size is a representation of the relative value of the reaction normalized to the maximum value that
         the edge can attain during the whole simulation.
+        Returns
+        -------
+        Three dictionaries. The first one contains the information of the edge sizes at all time points.
+        The second one contains the information of the edge colors at all time points.
+        The third one contains the values of the reaction rates at all time points.
         """
         all_rate_colors = {}
         all_rate_sizes = {}
@@ -191,13 +166,15 @@ class ModelVisualization(object):
         all_products = [rx['products'] for rx in self.model.reactions_bidirectional]
         all_reactants = [rx['reactants'] for rx in self.model.reactions_bidirectional]
         sp_imp = set(range(len(self.model.species))) - set(self.passengers)  # species with more than one edge
-        # TODO: Make sure that product nodes that also have more than one incoming node, don't overwrite previous nodes edges attrs
+        # TODO: Make sure that product nodes that also have more than one incoming node, don't overwrite
+        #  previous nodes edges attrs
 
         for sp in sp_imp:
             # Getting the indices of the reactions_bidirectional in which sp is a reactant and a product, respectively
             rxns_idx_reactant = [i for i, rx in enumerate(all_reactants) if sp in rx]
             rxns_idx_product = [i for i, rx in enumerate(all_products) if sp in rx]
             repeated_rxn = set(rxns_idx_reactant) & set(rxns_idx_product)
+            # Obtain indices of reactions in which sp is only a product
             rxns_idx_product = [c for c in rxns_idx_product if c not in repeated_rxn]
 
             # Getting arrays to obtain producing and consuming reactions
@@ -220,14 +197,6 @@ class ModelVisualization(object):
 
             if rxn_val_neg.size != 0:
                 rxn_val_neg_max = np.amax(rxn_val_neg) + self.mach_eps
-
-            # Tropicalization
-            # TODO: Find a better way, so the function doesnt have to check if maxplus is false or true at each step
-            # if maxplus:
-            #     sp_rr_dom = self._get_max_rrs(rxns_matrix[rxns_idx_reactant], 1)
-            # else:
-            #     sp_rr_dom = rxns_matrix[rxns_idx_reactant]
-            #     sp_prr_dom = rxns_matrix[rxns_idx_product]
 
             sp_rr_dom = rxns_matrix[rxns_idx_reactant]
             sp_prr_dom = rxns_matrix[rxns_idx_product]
@@ -306,8 +275,12 @@ class ModelVisualization(object):
 
     def node_data(self):
         """
-        Converts concentration values to colors to be used in the nodes
-        :return: Returns a pandas data frame where each row is a node and each column a time point, with the color value
+        Obtains the species concentration values and the relative concentration
+        compared with the maximum concentration across all time points
+        Returns
+        -------
+        Two dictionaries. The first one has the species concentration. The
+        second one has the relative species concentrations
         """
         node_absolute = {}
         node_relative = {}
@@ -324,12 +297,19 @@ class ModelVisualization(object):
     def f2hex_edges(fx, vmin=-0.99, vmax=0.99):
         """
         Converts reaction rates values to f2hex colors
-        :param fx: Vector of reaction rates (flux)
-        :param vmin: Value of minimum for normalization
-        :param vmax: Value of maximum for normalization
-        :return: This function returns a vector of colors in hex format that represents flux
-        """
+        Parameters
+        ----------
+        fx: vector-like
+            Vector of reaction rates(flux)
+        vmin: float
+            Value of the minimum for normalization
+        vmax: float
+            Value of the maximum for normalization
 
+        Returns
+        -------
+        A vector of colors in hex format that encodes the reaction rate values
+        """
         norm = MidpointNormalize(vmin=vmin, vmax=vmax, midpoint=0)
         f2rgb = cm.ScalarMappable(norm=norm, cmap=cm.get_cmap('RdBu_r'))
         rgb = [f2rgb.to_rgba(rate)[:3] for rate in fx]
@@ -341,13 +321,23 @@ class ModelVisualization(object):
     @staticmethod
     def range_normalization(x, min_x, max_x, a=0.5, b=10):
         """
-        Normalized vector to the [0.1,15] range
-        :param x: Vector of numbers to be normalized
-        :param min_x: Minimum value in vector x
-        :param max_x: Maximum value in vector x
-        :param a: Value of minimum used for the normalization
-        :param b: Value of maximum used for the normalization
-        :return: Normalized vector
+        Normalize vector to the [0.5, 10] range
+        Parameters
+        ----------
+        x: vector-like
+            Vector of values to be normalized
+        min_x: float
+            Minimum value in vector x
+        max_x: float
+            Maximum value in vector x
+        a: float
+            Value of minimum used for the normalization
+        b: float
+            Value of maximum used for the normalization
+
+        Returns
+        -------
+        Normalized vector
         """
         return a + (x - min_x) * (b - a) / (max_x - min_x)
 
