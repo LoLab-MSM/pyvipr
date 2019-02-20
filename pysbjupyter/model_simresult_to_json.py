ac4ec64841641947bc9282f9ae4acf6e41ed3b41
oscar <oo.ortega2247@gmail.com>
2019-02-20 12:30:00 -0600

    Refactor function to convert models and simulations to json

--- pysbjupyter/model_simresult_to_json.py
+++ pysbjupyter/model_simresult_to_json.py
@@ -8,33 +8,75 @@ import os
 
 
 def data_to_json(value, widget):
+    """
+    Generate a json file from the data passed to the widget
+    Parameters
+    ----------
+    value: pysb.Model, pysb.SimulationResult, str
+        Value passed to the widget that is going to be visualized
+    widget: Widget
+        Widget instance
 
-    if isinstance(value, Model):
-        viz = StaticViz(value)
+    Returns
+    -------
 
-    elif isinstance(value, str):
-        file_extension = os.path.splitext(value)[1]
-        if file_extension == '.bngl':
-            model = model_from_bngl(value)
-        elif file_extension == '.sbml':
-            model = model_from_sbml(value)
-        elif value.startswith('BIOMD'):
-            model = model_from_biomodels(value)
-        else:
-            raise ValueError('Format not supported')
-        viz = StaticViz(model)
+    """
+
+    if isinstance(value, (Model, str)):
+        viz = dispatch_pysb_files(value)
+        try:
+            if widget.type_of_viz == 'communities_view':
+                rs = widget.random_state
+                jsondata = getattr(viz, widget.type_of_viz)(random_state=rs)
+            else:
+                jsondata = getattr(viz, widget.type_of_viz)()
+        except AttributeError:
+            raise AttributeError('Type of static visualization not defined')
+        return jsondata
 
     elif isinstance(value, SimulationResult):
         viz = ModelVisualization(value)
-
+        process = widget.process
+        try:
+            if widget.type_of_viz == 'dynamic_communities_view':
+                rs = widget.random_state
+                jsondata = getattr(viz, widget.type_of_viz)(random_state=rs, type_viz=process)
+            else:
+                jsondata = getattr(viz, widget.type_of_viz)(type_viz=process)
+        except AttributeError:
+            raise AttributeError('Type of visualization not defined')
+        return jsondata
     else:
         raise TypeError('Only Model and SimulationResult are supported')
 
-    try:
-        jsondata = getattr(viz, widget.type_of_viz)()
-    except AttributeError:
-        raise AttributeError('Type of visualization not defined')
-    return jsondata
+
+def dispatch_pysb_files(value):
+    functions = {'str': _handle_model_files, 'model': _handle_pysb_model}
+    data_type = str(type(value)).split("'")[1]
+    result = functions[data_type](value)
+    return result
+
+
+def _handle_model_files(value):
+    file_extension = os.path.splitext(value)[1]
+    if file_extension == '.bngl':
+        model = model_from_bngl(value)
+    elif file_extension == '.sbml':
+        model = model_from_sbml(value)
+    elif value.startswith('BIOMD'):
+        model = model_from_biomodels(value)
+    else:
+        raise ValueError('Format not supported')
+    viz = StaticViz(model)
+    return viz
+
+
+def _handle_pysb_model(value):
+    return StaticViz(value)
+
+
+
+
 
 
 
