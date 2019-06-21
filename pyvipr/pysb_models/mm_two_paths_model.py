09e9d697e374d5db04c6b525ab70630c6d1b49fa
oscar <oo.ortega2247@gmail.com>
2019-06-21 11:35:45 -0500

    Use macros

--- pyvipr/pysb_models/mm_two_paths_model.py
+++ pyvipr/pysb_models/mm_two_paths_model.py
@@ -1,5 +1,5 @@
 from pysb import Model, Parameter, Monomer, Rule, Observable, Initial
-
+from pysb.macros import catalyze
 
 Model()
 #######
@@ -18,13 +18,15 @@ Monomer('S', ['e', 'type'], {'type': ['A', 'B']})
 Monomer('P')
 
 # Rules
-Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='A') | E(s=1) % S(e=1, type='A'), kf1, kr1)
-Rule('Production_1', E(s=1) % S(e=1, type='A') >> E(s=None) + P(), kcat1)
-Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='B') | E(s=1) % S(e=1, type='B'), kf2, kr2)
-Rule('Production_2', E(s=1) % S(e=1, type='B') >> E(s=None) + P(), kcat2)
+# Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='A') | E(s=1) % S(e=1, type='A'), kf1, kr1)
+# Rule('Production_1', E(s=1) % S(e=1, type='A') >> E(s=None) + P(), kcat1)
+# Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='B') | E(s=1) % S(e=1, type='B'), kf2, kr2)
+# Rule('Production_2', E(s=1) % S(e=1, type='B') >> E(s=None) + P(), kcat2)
 
 # Macro
-# catalyze_state(E(), 's', S(), 'e', 'state', '0', '1', [kf,kr,kcat])
+catalyze(E(), 's', S(type='A'), 'e', P(), [kf1,kr1,kcat1])
+catalyze(E(), 's', S(type='B'), 'e', P(), [kf2,kr2,kcat2])
+
 
 Observable("E_free",      E(s=None))
 Observable("S1_free",     S(e=None, type='A'))
