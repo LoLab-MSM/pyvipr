46a2e946d9c660c82bf4ec30c082e45d9df0918a
ortega2247 <oo.ortega2247@gmail.com>
2019-09-16 15:43:06 -0500

    tests for pysb static visualizations

---
+++ pyvipr/tests/test_pysb_static.py
@@ -0,0 +1,46 @@
+import pytest
+from pyvipr.examples_models.lopez_embedded import model
+from pyvipr.pysb_viz.static_viz import PysbStaticViz
+
+
+@pytest.fixture
+def viz_model():
+    viz = PysbStaticViz(model)
+    return viz
+
+
+def test_viz_exists(viz_model):
+    assert viz_model
+
+
+def test_graphs(viz_model):
+    g_sp = viz_model.species_graph()
+    g_rxn_bi = viz_model.sp_rxns_bidirectional_graph()
+    g_rxn = viz_model.sp_rxns_graph()
+    g_rules = viz_model.sp_rules_graph()
+    g_proj_sp = viz_model.projected_graph(g_rxn_bi, 'species_reactions')
+    g_proj_rxns = viz_model.projected_graph(g_rxn_bi, 'reactions')
+    g_proj_sp2 = viz_model.projected_graph(g_rules, 'species_rules')
+    g_proj_rules = viz_model.projected_graph(g_rules, 'rules')
+    g_comm = viz_model.communities_data_graph(random_state=1)
+
+    n_species = len(viz_model.model.species)
+    assert len(g_sp.nodes()) == n_species
+    assert len(g_rxn_bi.nodes()) == n_species + len(viz_model.model.reactions_bidirectional)
+    assert len(g_rxn.nodes()) == n_species + len(viz_model.model.reactions)
+    assert len(g_rules.nodes()) == n_species + len(viz_model.model.rules)
+    assert len(g_proj_sp.nodes()) == n_species
+    assert len(g_proj_rxns.nodes()) == len(viz_model.model.reactions_bidirectional)
+    assert len(g_proj_sp2.nodes()) == n_species
+    assert len(g_proj_rules.nodes()) == len(viz_model.model.rules)
+    assert len(g_comm.nodes(data='parent')) == n_species + 9
+
+
+def test_wrong_projection(viz_model):
+    with pytest.raises(ValueError):
+        viz_model._projections_view('wrong_projection')
+
+
+def test_no_compartments(viz_model):
+    with pytest.raises(ValueError):
+        viz_model.compartments_data_graph()
