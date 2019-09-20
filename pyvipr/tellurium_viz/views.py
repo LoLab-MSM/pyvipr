a3518eb802adeebde64b2150d38411029f57c426
ortega2247 <oo.ortega2247@gmail.com>
2019-09-20 16:06:34 -0500

    Add documentation to functions

--- pyvipr/tellurium_viz/views.py
+++ pyvipr/tellurium_viz/views.py
@@ -88,22 +88,117 @@ def sp_comm_louvain_view(model, layout_name='klay', random_state=None):
 
 
 def sp_comm_greedy_view(model, layout_name='klay'):
+    """
+    Render a visualization of the interactions between the species in a model.
+    The species nodes are grouped by the communities detected by the
+    Clauset-Newman-Moore greedy modularity maximization algorithm
+    implemented in Networkx
+
+    Parameters
+    ----------
+    model: pysb.model or str
+        Model to visualize. It can be a pysb model, or the file path to an
+        an SBML or BNGL model
+    layout_name: str
+        Layout to use
+
+    Returns
+    -------
+
+    """
     return Viz(data=model, type_of_viz='sp_comm_greedy_view', layout_name=layout_name)
 
 
-def sp_comm_asyn_lpa_view(model, layout_name='klay'):
-    return Viz(data=model, type_of_viz='sp_comm_asyn_lpa_view', layout_name=layout_name)
+def sp_comm_asyn_lpa_view(model, random_state=None, layout_name='klay'):
+    """
+    Render a visualization of the interactions between the species in a model.
+    The species nodes are grouped by the communities detected by the
+    asynchronous label propagation algorithm implemented in Networkx.
+
+    Parameters
+    ----------
+    model: pysb.model or str
+        Model to visualize. It can be a pysb model, or the file path to an
+        an SBML or BNGL model
+    layout_name: str
+        Layout to use
+    random_state: int
+        Random state seed use by the community detection algorithm
+
+    Returns
+    -------
+
+    """
+    return Viz(data=model, type_of_viz='sp_comm_asyn_lpa_view', layout_name=layout_name,
+               random_state=random_state)
 
 
 def sp_comm_label_propagation_view(model, layout_name='klay'):
+    """
+    Render a visualization of the interactions between the species in a model.
+    The species nodes are grouped by the communities detected by the
+    label propagation algorithm implemented in Networkx.
+
+    Parameters
+    ----------
+    model: pysb.model or str
+        Model to visualize. It can be a pysb model, or the file path to an
+        an SBML or BNGL model
+    layout_name: str
+        Layout to use
+
+    Returns
+    -------
+
+    """
     return Viz(data=model, type_of_viz='sp_comm_label_propagation_view', layout_name=layout_name)
 
 
 def sp_comm_girvan_newman_view(model, layout_name='klay'):
+    """
+    Render a visualization of the interactions between the species in a model.
+    The species nodes are grouped by the communities detected by the
+    Girvan-Newman method implemented in Networkx.
+
+    Parameters
+    ----------
+    model: pysb.model or str
+        Model to visualize. It can be a pysb model, or the file path to an
+        an SBML or BNGL model
+    layout_name: str
+        Layout to use
+
+    Returns
+    -------
+
+    """
     return Viz(data=model, type_of_viz='sp_comm_girvan_newman_view', layout_name=layout_name)
 
 
 def sp_comm_asyn_fluidc_view(model, k, max_iter=100, seed=None, layout_name='fcose'):
+    """
+    Render a visualization of the interactions between the species in a model.
+    The species nodes are grouped by the communities detected by the
+    asynchronous label propagation algorithm implemented in Networkx.
+
+    Parameters
+    ----------
+    model: pysb.model or str
+        Model to visualize. It can be a pysb model, or the file path to an
+        an SBML or BNGL model
+    k: int
+        The number of communities to be found
+    max_iter: int
+        The number of maximum iterations allowed
+    random_state: int
+        Random state seed use by the community detection algorithm
+    layout_name: str
+        Layout to use
+
+    Returns
+    -------
+
+    """
     from pyvipr.tellurium_viz.static_viz import TelluriumStaticViz
     tviz = TelluriumStaticViz(model)
     data = tviz.sp_comm_asyn_fluidc_view(k, max_iter, seed)
