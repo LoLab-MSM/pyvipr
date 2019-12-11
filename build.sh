3752b6481062af677ec63ce7eb084a88aa4c5eb1
Oscar Ortega <oo.ortega2247@gmail.com>
2019-12-11 14:57:15 -0600

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install -e ".[test, examples]" .
-jupyter nbextension install --py --symlink --sys-prefix pyvipr
-jupyter nbextension enable --py --sys-prefix pyvipr
-jupyter notebook
